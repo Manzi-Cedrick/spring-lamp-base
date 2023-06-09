19e23ff88a9f3fc8dc1825d4f4cc87ac9f883e30
Manzi-Cedrick <cedrickmanzii0@gmail.com>
2023-06-09 14:39:47 +0200

    [add]: controller request mapping

---
+++ src/main/java/rw/ac/rca/springmvc/controller/AddController.java
@@ -0,0 +1,32 @@
+package rw.ac.rca.springmvc.controller;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import rw.ac.rca.springmvc.service.CalculatorService;
+
+/**
+ * @author Manzi Cedrick
+ *
+ */
+@Controller
+public class AddController {
+    @RequestMapping("/add.php")
+    public ModelAndView add(HttpServletRequest request, HttpServletResponse response) {
+        int a = Integer.parseInt(request.getParameter("num1"));
+        int b = Integer.parseInt(request.getParameter("num2"));
+        CalculatorService calcService = new CalculatorService();
+        double sum = calcService.addTwoNumbers(a, b);
+        double prod = calcService.multiplyTwoNumbers(a, b);
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("results.jsp");
+        mv.addObject("fv", a);
+        mv.addObject("sv", b);
+        mv.addObject("theSum", sum);
+        mv.addObject("product", prod);
+        return mv;
+    }
+}
